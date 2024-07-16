e2ff8b2943c767010d8cf8924a140a0a2b9060ce
crusader <enememeka44@gmail.com>
2024-07-16 10:36:24 +0100

    added .vscode to .gitignore

--- BACKEND/app/models/colony.py
+++ BACKEND/app/models/colony.py
@@ -1,1 +1,10 @@
-'''module for defining the colony '''
+#./api/models/colony.py
+'''module for defining the `colony` models that will be used to perform CRUD operations on the database and
+The models that will be used as schemas/response/request data in the API schema. All SQLModels'''
+from sqlmodel import SQLModel, Field
+from app.models.players import PlayerInfo
+
+class BaseColony(SQLModel):
+    'base class for a colony'
+    number: int
+    players: list[PlayerInfo]
