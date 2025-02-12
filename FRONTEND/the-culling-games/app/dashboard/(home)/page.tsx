75c18b78209c894d221ec5607e339c5fda24a685
crusader <enememeka44@gmail.com>
2025-02-12 22:41:15 +0100

    Refactor Dashboard layout for improved responsiveness and organization

--- FRONTEND/the-culling-games/app/dashboard/(home)/page.tsx
+++ FRONTEND/the-culling-games/app/dashboard/(home)/page.tsx
@@ -10,18 +10,20 @@ export default function Dashboard() {
         <div className="flex flex-col justify-between border gap-3 p-4 container contain-content overflow-y-scroll">
             <div className="flex gap-2 flex-1">
                 <div className="w-2/3 flex flex-col gap-2 justify-between">
-                    <div className="flex flex-col md:flex-row gap-1">
-                        <div className="md:w-1/2">
-                            <h3 className="text-xs sm:text-lg">
-                                Current Match
-                            </h3>
-                            <LatestMatch />
-                        </div>
-                        <div className="md:w-1/2">
-                            <h3 className="text-xs sm:text-lg">
-                                Your Player
-                            </h3>
-                            <PlayerCard />
+                    <div className="flex flex-col gap-2 flex-1 justify-between">
+                        <div className="flex flex-col gap-1">
+                            <div className="md:w-1/2">
+                                <h3 className="text-xs sm:text-lg">
+                                    Current Match
+                                </h3>
+                                <LatestMatch />
+                            </div>
+                            <div className="md:w-1/2">
+                                <h3 className="text-xs sm:text-lg">
+                                    Your Player
+                                </h3>
+                                <PlayerCard />
+                            </div>
                         </div>
                         <div>
                             <ColonyPlayerGradeChart />
